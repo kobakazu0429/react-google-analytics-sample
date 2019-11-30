76f1e4f344b7e78a4fdb9ee61677ec5a98c9a905
kobakakzu0429 <kobakazu10@gmail.com>
2019-11-30 13:05:30 +0900

    done

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,86 @@
-import React from "react";
+import React, {
+  FC,
+  useCallback,
+  useState,
+  ChangeEvent,
+  useEffect
+} from "react";
+import { Route, Link, useLocation } from "react-router-dom";
+import ReactGA from "react-ga";
 
-const App = () => <p>Hello, React World !</p>;
+export const App: FC<{}> = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">to top</Link>
+          </li>
+          <li>
+            <Link to="/page1">to page 1</Link>
+          </li>
+        </ul>
+      </nav>
+      <Route
+        exact
+        path="/"
+        component={() => <Page title="top" description="this is top page" />}
+      />
+      <Route
+        exact
+        path="/page1"
+        component={() => <Page title="page 1" description="this is page 1" />}
+      />
+    </div>
+  );
+};
 
-export default App;
+interface IProps {
+  title: string;
+  description: string;
+}
+
+const Page: FC<IProps> = ({ title, description }) => {
+  const location = useLocation();
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location.pathname, location.search]);
+
+  const increment = useCallback(() => {
+    ReactGA.event({
+      category: "Test",
+      action: "Click increment button"
+    });
+  }, []);
+
+  const [input, setInput] = useState(0);
+  const inputChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const text = e.target.value;
+      if (!text) return;
+      Number.isInteger(Number(text));
+      console.log(text);
+      setInput(Number(text));
+    },
+    [input]
+  );
+  const sendInput = useCallback(() => {
+    ReactGA.event({
+      category: "Social",
+      action: "Rated an App",
+      label: "hoge",
+      value: input
+    });
+  }, [input]);
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <button onClick={increment}>increment button</button>
+      <hr />
+      <input type="number" onChange={inputChange} value={input} />
+      <button onClick={sendInput}>send your rate</button>
+    </div>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,13 @@ import "normalize.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-153638553-1", {
+  gaOptions: { cookieDomain: "none" },
+  debug: true
+});
 
-import App from "./components/App";
+import { App } from "./components/App";
 
 ReactDOM.render(
   <BrowserRouter>
